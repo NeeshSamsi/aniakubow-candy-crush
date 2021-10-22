3c03f3fd11642e5d257f01828315a250925a1d26
Neesh <avaneeshsamsi@gmail.com>
2021-10-22 21:40:31 +0530

    33:05 started looking for columns of three

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
 
-const size = 8;
+const width = 8;
 const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, (i + width) * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (columnOfThree.every((square) => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangement = [];
 
-    for (let i = 0; i < size * size; i++) {
+    for (let i = 0; i < width * width; i++) {
       const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
       randomColorArrangement.push(randomColor);
     }
@@ -25,7 +36,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="game"></div>
+      <div className="game">
+        {currentColorArrangement.map((candyColor, i) => (
+          <img key={i} style={{ backgroundColor: candyColor }} alt={`${candyColor} square`} />
+        ))}
+      </div>
     </div>
   );
 };
